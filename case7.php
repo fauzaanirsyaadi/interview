a0b7bc3c8df3bda1669565cfb82f10e2dbb3f009
fauzaan.irsyaadi <fauzaan.irsyaadi@gmail.com>
2021-07-19 17:28:18 +0700

    submit 2

--- case7.php
+++ case7.php
@@ -1,33 +1,69 @@
 <?php
+    $jumlahUang = 72500;
 
-class CaseSeven
-{
-    public function kembalian(int $total_uang)
-    {
-        error_reporting(0);
-        $pecahan = [100000, 75000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 25, 10, 1];
+    $a;$b;$c;$d;$e;$f;$g;$h;
+    
+    $Mod;
+    
+    
+    
+    //$b
+    
+    $Mod=$jumlahUang%50000;
+    
+    $b=($jumlahUang-$Mod)/50000;
+    
+    $jumlahUang=$jumlahUang-($b*50000);
+    
+    //$c
+    
+    $Mod=$jumlahUang%20000;
+    
+    $c=($jumlahUang-$Mod)/20000;
+    
+    $jumlahUang=$jumlahUang-($c*20000);
+    
+    //$d
+    
+    $Mod=$jumlahUang%10000;
+    
+    $d=($jumlahUang-$Mod)/10000;
+    
+    $jumlahUang=$jumlahUang-($d*10000);
+    
+    //$e
+    
+    $Mod=$jumlahUang%5000;
+    
+    $e=($jumlahUang-$Mod)/5000;
+    
+    $jumlahUang=$jumlahUang-($e*5000);
+    
+    //$f
+    
+    $Mod=$jumlahUang%1000;
+    
+    $f=($jumlahUang-$Mod)/1000;
+    
+    $jumlahUang=$jumlahUang-($f*1000);
 
-        if ($total_uang < $total_belanja) {
-            echo "Uang Kurang\n";
-        } elseif ($total_belanja == $total_uang) {
-            echo "Tidak Ada Kembalian\n";
-        } else {
-            $jumlah_kembalian = $total_uang - $total_belanja;
-            echo "Jumlah Kembalian = Rp. " . number_format($jumlah_kembalian) . "\n";
-            $jumlah_pecahan = count($pecahan);
-            for ($i = 0; $i <= $jumlah_pecahan; $i++) {
-                $genap = floor($jumlah_kembalian / $pecahan[$i]);
-                $sisa = $jumlah_kembalian % $pecahan[$i];
-                if ($genap > 0) {
-                    $jenis = ($pecahan[$i] > 1000) ? "Lembar" : "Keping";
-                    echo $genap . " " . $jenis . " " . "Uang Pecahan" . " Rp. " . number_format($pecahan[$i]) . "\n";
-                }
-                $jumlah_kembalian = $sisa;
-            }
-        }
-    }
-}
+    //$g
+    
+    $Mod=$jumlahUang%200;
+    
+    $g=($jumlahUang-$Mod)/200;
+    
+    $jumlahUang=$jumlahUang-($g*200);
 
-$CaseSeven = new CaseSeven();
+    //$h
+    
+    $Mod=$jumlahUang%100;
+    
+    $h=($jumlahUang-$Mod)/100;
+    
+    $jumlahUang=$jumlahUang-($h*100);
+    
+    
+    echo "50000 berjumlah : ".$b. ",20000 berjumlah :  " .$c. ",10000 berjumlah : " .$d. ",5000 berjumlah : " .$e. ",1000 berjumlah : " .$f. ",200 berjumlah : " .$g. ",100 berjumlah : " .$h;
 
-echo $CaseSeven->kembalian(72500);
+    ?>
